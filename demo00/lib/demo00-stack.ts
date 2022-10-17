b38ba4949f8f8f6c4b258d8075687197a68aab77
greensmith <greensmith+github@gmail.com>
2022-10-17 20:38:21 +0100

    update 1

---
+++ demo00/lib/demo00-stack.ts
@@ -0,0 +1,20 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Demo00Stack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'Demo00Queue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    const mybucket = new Bucket(this,'Bucket');
+
+  }
+}
---
+++ demo00/test/demo00.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Demo00 from '../lib/demo00-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/demo00-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Demo00.Demo00Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
